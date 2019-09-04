7a621df5794a4920751b3b1863eb34ab3194f462
Mark Mankarious <mz.mankarious@gmail.com>
2019-09-05 00:29:35 +0100

    Updates deploy config

--- deploy/scripts/AfterInstall.sh
+++ deploy/scripts/AfterInstall.sh
@@ -9,13 +9,15 @@ cd ~
 $(aws ecr get-login --region eu-west-2 --no-include-email)
 
 # PULL BACKEND IMAGE
-docker pull 647399693225.dkr.ecr.eu-west-2.amazonaws.com/grademypaper-backend:latest
 docker stop grademypaper-backend || true
 docker rm grademypaper-backend || true
+docker image rm 647399693225.dkr.ecr.eu-west-2.amazonaws.com/grademypaper-backend:latest || true
+docker pull 647399693225.dkr.ecr.eu-west-2.amazonaws.com/grademypaper-backend:latest
 docker run --env-file .env-backend --network grademypaper_network -d -p 4000:4000 --name grademypaper-backend 647399693225.dkr.ecr.eu-west-2.amazonaws.com/grademypaper-backend:latest
 
 # PULL FRONTEND IMAGE
-docker pull 647399693225.dkr.ecr.eu-west-2.amazonaws.com/grademypaper-frontend:latest
 docker stop grademypaper-frontend || true
 docker rm grademypaper-frontend || true
+docker image rm 647399693225.dkr.ecr.eu-west-2.amazonaws.com/grademypaper-frontend:latest || true
+docker pull 647399693225.dkr.ecr.eu-west-2.amazonaws.com/grademypaper-frontend:latest
 docker run -d -p 3000:3000 --name grademypaper-frontend 647399693225.dkr.ecr.eu-west-2.amazonaws.com/grademypaper-frontend:latest
