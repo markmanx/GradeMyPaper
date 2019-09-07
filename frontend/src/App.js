a8c701ed53a187b56b3fed6ad0c661bc384e2c05
Mark Mankarious <mz.mankarious@gmail.com>
2019-09-07 23:51:06 +0100

    Adds themeProviders for styled components and mui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components/macro';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/styles';
 
 import { Auth0Provider } from './context/auth0';
 import { Provider as ApolloProvider } from './helpers/apolloClient';
 import { RouteMatcher } from './helpers/routes';
+import { theme } from './helpers/themeHelpers';
 import { Header } from './components';
 
 function App() {
@@ -14,8 +17,12 @@ function App() {
         redirect_uri={window.location.origin}
         audience={`https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/`}
       >
-        <Header />
-        <RouteMatcher />
+        <ThemeProvider theme={theme}>
+          <MuiThemeProvider theme={theme}>
+            <Header />
+            <RouteMatcher />
+          </MuiThemeProvider>
+        </ThemeProvider>
       </Auth0Provider>
     </ApolloProvider>
   );
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import styled, { ThemeContext } from 'styled-components/macro';
 import { Link } from 'react-router-dom';
 
 export const HomeScreen = () => {
+  const theme = React.useContext(ThemeContext);
+  console.log(theme);
+
   return (
     <div>
       This is the home screen <Link to="/protected">Go to protected route</Link>
